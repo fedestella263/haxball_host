faa6a5e5cd345260f1f8070ead8e114a2dad3e75
Federico <fedestella263@gmail.com>
2019-08-29 23:22:47 -0300

    Se mejora el codigo

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var room = HBInit({
-    roomName: "IOGAME.IO - BIG",
-    maxPlayers: 12,
+    roomName: "BIG 3v3",
+    maxPlayers: 20,
     noPlayer: true,
-    public: false,
-    token: "thr1.AAAAAF1fVDZP5pUZt52cDw.kq_0vQidYXs"
+    public: true,
+    token: 'thr1.AAAAAF1ogB_hSBZSuZ4OzA.dt698FroZHM'
 });
 
-var leaders = ["🅒⓿🅝🅓⓿🅡"]
+var ignore_players = [];
+var last_players_activity = {}
+
+const leaders = ["🅒⓿🅝🅓⓿🅡"];
+const seconds_to_remove_afk_admins = 60*10;
+const time_to_clear_bans = 1000*60*60;
 
 room.setDefaultStadium("Big");
 room.setScoreLimit(3);
@@ -18,54 +23,74 @@ function isLeaderPlayer(player) {
 }
 
 function removeAdmins() {
-    for(const player of room.getPlayerList().find((player) => !isLeaderPlayer(player)))
-        room.setPlayerAdmin(player.id, false);
+    for(const player of room.getPlayerList()) {
+        if(player.admin && !isLeaderPlayer(player))
+            room.setPlayerAdmin(player.id, false);
+    }
 }
 
 function updateAdmins() { 
     var players = room.getPlayerList();
 
+    console.log(players)
+
     if(players.length == 0)
         return;
-
-    if(players.find((player) => player.admin) != null) {
-        player = players.find((player) => isLeaderPlayer(player));
-
-        if(player != null) {
-            removeAdmins();
-            room.setPlayerAdmin(player.id, true);
-            room.sendAnnouncement(`Bienvenido ${player.name} nuestro lider supremo!`)
-        }
-        
+    
+    // Esta el leader y no es administrador.
+    var leader_player = players.find((player) => isLeaderPlayer(player) && !player.admin && !ignore_players.includes(player.id));
+    if(leader_player != null) {
+        removeAdmins();
+        room.setPlayerAdmin(leader_player.id, true);
         return;
     }
 
-    room.setPlayerAdmin(players[0].id, true);
+    // Si ya hay un administrador que no esta ignorado sale.
+    if(players.find((player) => player.admin && !ignore_players.includes(player.id)) != null)
+        return;
+
+    // Asigna el primer jugador que no este en la lista de ignorar.
+    for(const player of room.getPlayerList()) {
+        if(!ignore_players.includes(player.id)) {
+            room.setPlayerAdmin(player.id, true);
+            break;
+        }
+    }
 }
 
 room.onPlayerJoin = function(player) {
-    room.sendAnnouncement(`Bienvenido ${player.name}!`)
+    room.onPlayerActivity(player);
+
+    if(isLeaderPlayer(player))
+        room.sendAnnouncement(`Bienvenido ${player.name} nuestro lider supremo!`);
+    else
+        room.sendAnnouncement(`Bienvenido ${player.name}!`);
+
     updateAdmins();
 }
 
 room.onPlayerLeave = function(player) {
+    delete last_players_activity[player.id];
     updateAdmins();
 }
 
-room.onTeamGoal = function(team_id) {
-    if(team_id == 1)
-        room.sendAnnouncement("Gggooooooooollll del equipo Rojo!!!");
-    else if(team_id == 2)
-        room.sendAnnouncement("Gggooooooooollll del equipo Azul!!!");
-}
+room.onTeamGoal = function(team_id) {}
 
 room.onTeamVictory = function(scores) {
     if(scores.red > scores.blue)
         room.sendAnnouncement(`Gana el equipo Rojo! Resultado: ${scores.red}-${scores.blue}`);
     else if(scores.red < scores.blue)
         room.sendAnnouncement(`Gana el equipo Azul! Resultado: ${scores.red}-${scores.blue}`);
+}
 
-    console.log(scores);
+
+room.onPlayerChat = function(player) {
+    room.onPlayerActivity(player);
+}
+
+room.onPlayerActivity = function(player) {
+    last_players_activity[player.id] = Date.now();
+    ignore_players = ignore_players.filter((id) => player.id == id);
 }
 
 room.onRoomLink = function(url) {
@@ -73,8 +98,36 @@ room.onRoomLink = function(url) {
     window.open(url);
 }
 
+room.onPlayerKicked = function(player, reason, ban, by_player) {
+    // Baneo a un lider.
+    if(isLeaderPlayer(player) && by_player != null) {
+        room.clearBan(player.id);
+        room.kickPlayer(by_player.id, 'Pelotudo no podes banear al dueño del host', true);        
+    }
+}
+
+// Borra los bans cada una hora.
 setInterval(function() {
+    console.log('Eliminando Bans');
     room.clearBans();
-}, 1000*60*60);
+}, time_to_clear_bans);
+
+// Quita el admin a los administradores inactivos mas de 10 minutos.
+setInterval(function() {
+    current_time = Date.now();
+    for(const player of room.getPlayerList()) {
+        var diff = (current_time-last_players_activity[player.id])/1000;
+
+        if(!player.admin)
+            continue;
+        
+        if(diff > seconds_to_remove_afk_admins) {
+            room.setPlayerAdmin(player.id, false);
+            ignore_players.push(player.id);
+            room.sendAnnouncement(`${player.name} se te removio el admin por pasar mucho tiempo afk`);
+            updateAdmins();
+        }
+    }
+}, 1000*1);
 
 console.clear();
